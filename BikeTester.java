144cef90bb1fe584c8c240f4b1a2c2365fcfdc3f
MartinLD11 <111070187+MartinLD11@users.noreply.github.com>
2022-09-23 14:59:59 -0400

    Update BikeTester.java

--- BikeTester.java
+++ BikeTester.java
@@ -1,3 +1,23 @@
+/**
+Name: Luke Martin
+Class: Itec 2150
+Description: Bike Class
+Create a business class called Bike
+Bikes should at least have number of wheels, manufacturer and year data. Additional data can be created at the programmer’s discretion.
+A no-arg constructor with values of 2 for number of wheels to 2, “Schwinn” for manufacturer, and 2014 for year should eb created.
+Other constructors, getters, setters and toString() method should be created.
+BikeTester Class
+Use the BikeTester class to create object bike1 using no-arg constructor. Print its details after creation.
+Create object bike2 in the same manner, and use the setter methods to change the instance variable values to those of your choice.
+Print details of bike2.
+Create an arraylist of bikes called bikeList.
+Add bike1 and bike2 to this list
+SOP bikeList
+Count the total number of wheels on all bikes in the bikeList and print them
+Find the year of the oldest bike in the list and print that year.
+Grade: 
+*/
+
 package Bike;
 import java.util.ArrayList;
 
